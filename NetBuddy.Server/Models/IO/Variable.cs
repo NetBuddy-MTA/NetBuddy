8ae6aacbe434cccf7fe95740974c3b33ce0ea313
OE100 <omer.er10@gmail.com>
2024-03-28 23:39:57 +0200

    added input/output representation

---
+++ NetBuddy.Server/Models/IO/Variable.cs
@@ -0,0 +1,14 @@
+﻿using Marten.Schema;
+
+namespace NetBuddy.Server.Models.IO;
+
+public class Variable
+{
+    [Identity]
+    public Guid Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public string Type { get; set; } = string.Empty;
+    public string Description { get; set; } = string.Empty;
+    public string DefaultValue { get; set; } = string.Empty;
+    public bool Required { get; set; } = false;
+}
