1780b3fe50558330ff0c3976688c6d13480d8e5b
OE100 <omer.er10@gmail.com>
2024-03-28 19:15:50 +0200

    added implementation for the password hash service

--- NetBuddy.Server/Services/BCryptPasswordService.cs
+++ NetBuddy.Server/Services/BCryptPasswordService.cs
@@ -5,13 +5,8 @@ namespace NetBuddy.Server.Services;
 
 public class BCryptPasswordService : IPasswordService
 {
-    public string Hash(string password)
-    {
-        var salt = GenerateSalt();
+    public string Hash(string password) => HashPassword(password);
 
-        return HashPassword(password, salt);
-    }
-
-    public bool Verify(string enteredPassword, string hashedPassword) =>
+    public bool Verify(string enteredPassword, string hashedPassword) => 
         BCrypt.Net.BCrypt.Verify(enteredPassword, hashedPassword);
 }
