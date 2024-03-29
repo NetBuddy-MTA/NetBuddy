b9a91f77caef09b3b87b30ac0da596775fd22c8f
OE100 <omer.er10@gmail.com>
2024-03-29 15:14:12 +0300

    the building block for sequences

---
+++ NetBuddy.Server/Models/Executables/Action.cs
@@ -0,0 +1,26 @@
+﻿using Marten.Schema;
+using NetBuddy.Server.Models.IO;
+
+namespace NetBuddy.Server.Models.Executables;
+
+public sealed class Action
+{
+    // Id is the primary key
+    [Identity] 
+    public Guid Id { get; set; }
+    
+    // Name is the display name
+    public string Name { get; set; } = string.Empty;
+    
+    // Description is the description of the action
+    public string Description { get; set; } = string.Empty;
+    
+    // Inputs is the list of inputs for the action
+    public List<Variable> Inputs { get; set; } = [];
+    
+    // Outputs is the list of outputs for the action
+    public List<Variable> Outputs { get; set; } = [];
+    
+    // The actual typescript/javascript code to be executed on the client
+    public string Code { get; set; } = string.Empty;
+}
