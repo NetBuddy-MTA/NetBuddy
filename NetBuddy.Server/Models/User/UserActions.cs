8add882d89b5fe5e4edadeed1b6882f04154ce3e
OE100 <omer.er10@gmail.com>
2024-03-29 15:28:53 +0300

    added a way to store the users' assigned actions for the database

---
+++ NetBuddy.Server/Models/User/UserActions.cs
@@ -0,0 +1,11 @@
+﻿using Marten.Schema;
+
+namespace NetBuddy.Server.Models.User;
+
+public sealed class UserActions
+{
+    [Identity]
+    public string Email { get; set; } = string.Empty;
+    
+    public List<Guid> ActionGuids { get; set; } = [];
+}
