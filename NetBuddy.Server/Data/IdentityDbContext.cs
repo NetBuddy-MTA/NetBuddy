741eaeb323d433e1f3fd1e9224fd35286cb67351
OE100 <omer.er10@gmail.com>
2024-04-02 15:54:34 +0300

    the db context for identity framework

---
+++ NetBuddy.Server/Data/IdentityDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using NetBuddy.Server.Models.User;
+
+namespace NetBuddy.Server.Data;
+
+public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+{
+    public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
+    {
+    }
+}
