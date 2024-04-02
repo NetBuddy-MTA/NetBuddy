4e067cc633b63e9abe4d4ffcd4ba2be0035f31cb
OE100 <omer.er10@gmail.com>
2024-04-02 17:41:39 +0300

    added limitations on user info

--- NetBuddy.Server/DTOs/Account/RegisterDto.cs
+++ NetBuddy.Server/DTOs/Account/RegisterDto.cs
@@ -5,12 +5,16 @@ namespace NetBuddy.Server.DTOs.Account;
 public sealed class RegisterDto
 {
     [Required]
+    [MaxLength(25)]
     public string? UserName { get; set; }
     
     [Required]
     [EmailAddress]
+    [MaxLength(50)]
     public string? Email { get; set; }
     
-    [Required] 
+    [Required]
+    [MinLength(6)]
+    [MaxLength(100)]
     public string? Password { get; set; }
 }
