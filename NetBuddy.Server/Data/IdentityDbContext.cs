2d0a7c38f94a3a3681929a59498d11c043cde4b6
OE100 <omer.er10@gmail.com>
2024-04-02 17:19:10 +0300

    added roles that can be assigned to users

--- NetBuddy.Server/Data/IdentityDbContext.cs
+++ NetBuddy.Server/Data/IdentityDbContext.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using NetBuddy.Server.Models.User;
 
 namespace NetBuddy.Server.Data;
 
-public class IdentityDbContext : IdentityDbContext<ApplicationUser>
+public class IdentityDbContext : IdentityDbContext<UserAccount>
 {
     public IdentityDbContext(DbContextOptions<IdentityDbContext> options) : base(options)
     {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        // create the roles
+        List<IdentityRole> roles =
+        [
+            new IdentityRole
+            {
+                Name = "Admin",
+                NormalizedName = "ADMIN"
+            },
+            new IdentityRole
+            {
+                Name = "User",
+                NormalizedName = "USER"
+            }
+        ];
+
+        builder.Entity<IdentityRole>().HasData(roles);
     }
 }
