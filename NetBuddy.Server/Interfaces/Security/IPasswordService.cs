f0fe0716e6a8a2291830bcf405121b1a9e626752
OE100 <omer.er10@gmail.com>
2024-04-02 17:44:43 +0300

    removed old password service interface

--- NetBuddy.Server/Interfaces/Security/IPasswordService.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace NetBuddy.Server.Interfaces.Security;
-
-public interface IPasswordService
-{
-    string Hash(string password);
-    bool Verify(string enteredPassword, string hashedPassword);
-}
