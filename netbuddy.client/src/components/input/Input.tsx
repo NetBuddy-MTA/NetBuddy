645abf8e94ff8b3115eb484fcb6de2d4bfd4cb2e
OE100 <omer.er10@gmail.com>
2024-04-06 20:20:49 +0300

    defined generic input box with label for testing

---
+++ netbuddy.client/src/components/input/Input.tsx
@@ -0,0 +1,28 @@
+﻿interface InputProps {
+  label: string;
+  type: string;
+  id: string;
+  placeholder: string;
+  setValue?: (value: string) => void;
+}
+
+const Input = ({label, type, id, placeholder, setValue}: InputProps) => {
+  return (
+    <div className="flex flex-col w-full gap-2">
+      <div className="flex justify-between">
+        <label htmlFor={id} className="font-semibold capitalize">
+          {label}
+        </label>
+      </div>
+      <input
+        id={id}
+        type={type}
+        className="w-full p-5 font-medium border rounded-md border-slate-300 placeholder:opacity-60"
+        placeholder={placeholder}
+        onChange={(e) => setValue && setValue(e.target.value)}
+      />
+    </div>
+  );
+}
+
+export default Input;
