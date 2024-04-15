702e8298b4682d5f63270f0d129d1423c0d60d7a
OE100 <omer.er10@gmail.com>
2024-04-15 18:47:50 +0300

    changed variable names to match scheme

--- netbuddy.client/src/api/auth/userAuth.ts
+++ netbuddy.client/src/api/auth/userAuth.ts
@@ -1,10 +1,10 @@
 ﻿const BASE_URL = 'https://localhost:7298/api/account';
 
-export const register = async (username: string, email: string, password: string) => {
+export const register = async (UserName: string, Email: string, Password: string) => {
   return await fetch(`${BASE_URL}/register/`, {
     method: 'POST',
     headers: new Headers({'Content-Type': 'application/json'}),
-    body: JSON.stringify({username, email, password}),
+    body: JSON.stringify({UserName, Email, Password}),
     mode: 'cors'
   })
     .catch((error) => console.log(error));
@@ -14,17 +14,17 @@ export const login = async (UserName: string, Password: string) => {
   return await fetch(`${BASE_URL}/login/`, {
     method: 'POST',
     headers: new Headers({'Content-Type': 'application/json'}),
-    body: JSON.stringify({username: UserName, password: Password}),
+    body: JSON.stringify({UserName, Password}),
     mode: 'cors'
   })
     .catch((error) => console.log(error));
 }
 
-export const deleteAccount = async (username: string, password: string) => {
+export const deleteAccount = async (UserName: string, Password: string) => {
   return await fetch(`${BASE_URL}/delete/`, {
     method: 'DELETE',
     headers: new Headers({'Content-Type': 'application/json'}),
-    body: JSON.stringify({username, password}),
+    body: JSON.stringify({UserName, Password}),
     mode: 'cors'
   })
     .catch((error) => console.log(error));
