4d86748c611a5b8058401a9c1b3042157fd8889a
OE100 <omer.er10@gmail.com>
2024-04-17 21:20:51 +0300

    split api into separate files, using axios agent to make requests

---
+++ netbuddy.client/src/api/Account/SignIn.ts
@@ -0,0 +1,15 @@
+﻿import {agentNoCredentials as agent} from '../agent.ts';
+
+export type LoginResponse = {
+  email: string
+  username: string
+  token: string
+}
+
+export default async function(UserName: string, Password: string) {
+  return await agent.post(
+    '/account/login',
+    JSON.stringify({UserName, Password}))
+    .catch(error => console.log(error.error))
+    .then(response => response?.data as void | LoginResponse);
+}
---
+++ netbuddy.client/src/api/Account/SignUp.ts
@@ -0,0 +1,15 @@
+﻿import {agentNoCredentials as agent} from '../agent.ts';
+
+export type SignupResponse = {
+  email: string
+  username: string
+  token: string
+}
+
+export default async function(UserName: string, Email: string, Password: string){
+  return await agent.post(
+    '/account/register',
+    JSON.stringify({UserName, Email, Password}))
+    .catch(error => console.log(error.error))
+    .then(response => response?.data as void | SignupResponse);
+}
