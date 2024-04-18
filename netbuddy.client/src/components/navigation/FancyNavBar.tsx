32816e20d946456a514ce94cb3eca6d448c41a4f
OE100 <omer.er10@gmail.com>
2024-04-18 13:53:36 +0300

    nicer looking app bar

--- netbuddy.client/src/components/navigation/FancyNavBar.tsx
+++ netbuddy.client/src/components/navigation/FancyNavBar.tsx
@@ -3,6 +3,8 @@ import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import MenuIcon from '@mui/icons-material/Menu';
 import {Link} from 'react-router-dom';
+import Stack from '@mui/material/Stack';
+import Typography from '@mui/material/Typography';
 
 export type PageAndLink = {
   page: string
@@ -18,17 +20,30 @@ const FancyNavBar = ({logo, pageAndLinks} : FancyNavBarProps) => {
   return (
     <AppBar position="static" sx={{px: 0, mx:0}}>
       <Toolbar sx={{px: 0, mx: 0}}>
-        <Button sx={{paddingLeft: 0}}>
-          <MenuIcon/>
-        </Button>
-        {logo ?? <img src={logo} alt="NetBuddyLogo" width={48} height={48}/>}
-        {pageAndLinks?.map(pageAndLink => {
-          return (
-            <Button key={pageAndLink.link} component={Link} to={pageAndLink.link} variant="text">
-              {pageAndLink.page}
-            </Button>
-          );
-        })}
+        <Stack direction="row" spacing={3}>
+          <Button>
+            <MenuIcon/>
+          </Button>
+          <Link to="/" style={{textDecoration: 'none', color: 'inherit'}}>
+            <Stack direction="row" alignItems="center" spacing={1}>
+              {logo ? <img src={logo} alt="NetBuddyLogo" width={48} height={48}/> : null}
+              <Typography
+                variant="h6"
+                noWrap 
+                fontWeight={"600"}
+              >
+                NetBuddy
+              </Typography>
+            </Stack>
+          </Link>
+          {pageAndLinks?.map(pageAndLink => {
+            return (
+              <Button key={pageAndLink.link} component={Link} to={pageAndLink.link} variant="text" sx={{fontWeight: '600'}}>
+                {pageAndLink.page}
+              </Button>
+            );
+          })}
+        </Stack>
       </Toolbar>
     </AppBar>
   );
