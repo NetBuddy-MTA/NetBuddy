78882d10eff6dc5015c554f7e50ed2bb31bad8c1
OE100 <omer.er10@gmail.com>
2024-04-19 16:04:04 +0300

    basic empty home screen for when you're signed in

--- netbuddy.client/src/screens/Home.tsx
+++ netbuddy.client/src/screens/Home.tsx
@@ -1,5 +1,18 @@
+import { useContext } from "react";
+import UserInfoContext from "../contexts/UserInfoContext.tsx";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+
 const Home = () => {
-  return <div></div>
+  const {userInfo} = useContext(UserInfoContext)
+  
+  return (
+    <Stack direction="column">
+      <Typography>
+        {`Hello ${userInfo?.username}`}
+      </Typography>
+    </Stack>
+  );
 };
 
 export default Home;
