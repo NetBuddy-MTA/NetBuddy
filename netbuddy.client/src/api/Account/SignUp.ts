69c92b79f7d8db45d362dea852b1fc5db7ab14a8
OE100 <omer.er10@gmail.com>
2024-04-20 14:40:23 +0300

    updated api to reflect new token policy

--- netbuddy.client/src/api/Account/SignUp.ts
+++ netbuddy.client/src/api/Account/SignUp.ts
@@ -1,15 +1,16 @@
-﻿import {agentNoCredentials as agent} from '../agent.ts';
-
-export type SignupResponse = {
-  email: string
-  username: string
-  token: string
-}
+﻿import { AxiosError } from 'axios';
+import {agentNoCredentials as agent} from '../agent.ts';
 
 export default async function(UserName: string, Email: string, Password: string){
   return await agent.post(
     '/account/register',
     JSON.stringify({UserName, Email, Password}))
-    .catch(error => console.log(error.error))
-    .then(response => response?.data as void | SignupResponse);
+    .catch((reason: AxiosError) => {
+      if (reason.response!.status === 400) {
+        alert("One of the fields doesn't match the requirements!");
+      }
+      else if (reason.response!.status === 500) {
+        alert("User couldn't be created!");
+      }
+    });
 }
