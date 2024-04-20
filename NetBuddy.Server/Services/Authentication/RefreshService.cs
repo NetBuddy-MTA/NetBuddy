38901eead15a1fc259d71d1caab3470e4915cbf4
OE100 <omer.er10@gmail.com>
2024-04-20 23:36:43 +0300

    added refresh token class and service

---
+++ NetBuddy.Server/Services/Authentication/RefreshService.cs
@@ -0,0 +1,18 @@
+﻿using System.Security.Cryptography;
+using NetBuddy.Server.Interfaces.Authentication;
+using NetBuddy.Server.Models.Auth;
+
+namespace NetBuddy.Server.Services.Authentication;
+
+public class RefreshService : IRefreshService
+{
+    public RefreshToken CreateRefresh()
+    {
+        return new RefreshToken
+        {
+            Token = Convert.ToBase64String(RandomNumberGenerator.GetBytes(64)),
+            Expires = DateTime.UtcNow.AddDays(30),
+            Created = DateTime.UtcNow
+        };
+    }
+}
