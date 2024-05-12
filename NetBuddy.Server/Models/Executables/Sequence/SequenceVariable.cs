0437a564b5b22d1f0a77b847ca19dd1e0f64f396
OE100 <omer.er10@gmail.com>
2024-05-12 18:47:26 +0300

    data structure containing all the information the extension needs of a sequence variable

---
+++ NetBuddy.Server/Models/Executables/Sequence/SequenceVariable.cs
@@ -0,0 +1,19 @@
+﻿using Marten.Schema;
+
+namespace NetBuddy.Server.Models.Executables.Sequence;
+
+public sealed class SequenceVariable
+{
+    [Identity]
+    public Guid Id;
+    
+    public string OriginalName { get; set; } = string.Empty;
+    
+    public string Name { get; set; } = string.Empty;
+    
+    public string Type { get; set; } = string.Empty;
+    
+    public bool Optional { get; set; } = false;
+    
+    public string? DefaultValue { get; set; } = null;
+}
