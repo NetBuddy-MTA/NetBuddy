ac434fcb0a3c96d5bdcaabe7f8aefc1e88ceebcb
OE100 <omer.er10@gmail.com>
2024-05-13 13:23:32 +0300

    added frontend api to request all sequences of a user for display, or one sequence with all it's info for editing, also the data type can be used to upload a sequence after the user created it to the server.

---
+++ netbuddy.client/src/api/sequences/sequences.ts
@@ -0,0 +1,42 @@
+﻿import {agentWithCredentials as agent} from "../agent.ts";
+
+export type SequenceDisplay = {
+  id: string;
+  name: string;
+  description: string;
+};
+
+export type SequenceVariable = {
+  id: string;
+  originalName: string;
+  name: string;
+  type: string;
+  optional: boolean;
+  defaultValue?: string;
+};
+
+export type ExecutableAction = {
+  id: string;
+  actionString: string;
+  inputs: SequenceVariable[];
+  outputs: SequenceVariable[];
+};
+
+export type Sequence = {
+  id: string;
+  name: string;
+  description: string;
+  actions: ExecutableAction[];
+};
+
+export async function GetSequencesDisplay() {
+  return await agent.get("execution/sequences")
+  .catch(error => console.log(error.error))
+  .then(response => response?.data as SequenceDisplay[]);
+}
+
+export async function GetExecutableSequence(id: string) {
+  return await agent.get(`execution/sequences/${id}`)
+  .catch(error => console.log(error.error))
+  .then(response => response?.data as Sequence);
+}
