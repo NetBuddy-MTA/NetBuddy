c459ed134c87b3d32ed7bd2187653b78dccd2b4f
OE100 <omer.er10@gmail.com>
2024-05-18 15:53:36 +0300

    improved code

--- netbuddy.client/src/screens/sequence/builder/ExecutableActionPropertiesView.tsx
+++ netbuddy.client/src/screens/sequence/builder/ExecutableActionPropertiesView.tsx
@@ -1,6 +1,9 @@
 import Paper from "@mui/material/Paper";
-import {ExecutableAction, SequenceVariable} from "../../../api/sequences/sequences.ts";
-import {useEffect} from "react";
+import {
+  ExecutableAction,
+  SequenceVariable,
+} from "../../../api/sequences/sequences.ts";
+import { useEffect } from "react";
 import Accordion from "@mui/material/Accordion";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import AccordionDetails from "@mui/material/AccordionDetails";
@@ -10,58 +13,59 @@ import ExpandMore from "@mui/icons-material/ExpandMore";
 import Grid from "@mui/material/Grid";
 
 const ExecutableActionPropertiesView = (props: {
-  selection: ExecutableAction | undefined, setSelection: (selection: ExecutableAction) => void
+  selection: ExecutableAction | undefined;
+  setSelection: (selection: ExecutableAction) => void;
 }) => {
-  const {selection} = props;
-  
-  useEffect(() => {
-  }, [selection]);
-  
+  const { selection } = props;
+
+  useEffect(() => {}, [selection]);
+
   const mapVariable = (variable: SequenceVariable) => {
     return (
       <Grid container key={variable.originalName} direction="column">
-        <Grid item>
-          {variable.originalName}
-        </Grid>
+        <Grid item>{variable.originalName}</Grid>
       </Grid>
     );
-  }
-  
+  };
+
+  const inputComponents = selection ? selection.inputs.map(mapVariable) : null;
+  const outputComponents = selection
+    ? selection.outputs.map(mapVariable)
+    : null;
+
   return (
     <Paper elevation={4}>
       <Typography variant="h4" p={1}>
         Properties:
       </Typography>
-      <Paper elevation={4}>
+      {(inputComponents ?? []).length > 0 ? (
         <Accordion>
-          <AccordionSummary expandIcon={<ExpandMore/>}>
-            <Tooltip title="Inputs are the data that the action needs to run." followCursor={true}>
-              <Typography variant="h6">
-                Inputs:
-              </Typography>
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            <Tooltip
+              title="Inputs are the data that the action needs to run."
+              followCursor={true}
+            >
+              <Typography variant="h6">Inputs:</Typography>
             </Tooltip>
           </AccordionSummary>
-          <AccordionDetails>
-            {selection ? selection.inputs.map(mapVariable) : null}
-          </AccordionDetails>
+          <AccordionDetails>{inputComponents}</AccordionDetails>
         </Accordion>
-      </Paper>
-      <Paper elevation={4}>
+      ) : null}
+      {(outputComponents ?? []).length > 0 ? (
         <Accordion>
-          <AccordionSummary expandIcon={<ExpandMore/>}>
-            <Tooltip title="Outputs are the data that the action produces." followCursor={true}>
-              <Typography variant="h6">
-                Outputs:
-              </Typography>
+          <AccordionSummary expandIcon={<ExpandMore />}>
+            <Tooltip
+              title="Outputs are the data that the action produces."
+              followCursor={true}
+            >
+              <Typography variant="h6">Outputs:</Typography>
             </Tooltip>
           </AccordionSummary>
-          <AccordionDetails>
-            {selection ? selection.outputs.map(mapVariable) : null}
-          </AccordionDetails>
+          <AccordionDetails>{outputComponents}</AccordionDetails>
         </Accordion>
-      </Paper>
+      ) : null}
     </Paper>
   );
-}
+};
 
 export default ExecutableActionPropertiesView;
