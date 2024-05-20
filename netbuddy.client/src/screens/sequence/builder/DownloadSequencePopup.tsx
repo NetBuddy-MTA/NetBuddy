58d4f412ffbd65d2ea6d69a18643cf028e84b889
OE100 <omer.er10@gmail.com>
2024-05-20 12:22:04 +0300

    added spacing to sequence download popup to make it more readable

--- netbuddy.client/src/screens/sequence/builder/DownloadSequencePopup.tsx
+++ netbuddy.client/src/screens/sequence/builder/DownloadSequencePopup.tsx
@@ -3,7 +3,6 @@ import {GetSequencesDisplay, SequenceDisplay} from "../../../api/sequences/seque
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
 import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
 import Typography from "@mui/material/Typography";
 import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
@@ -12,6 +11,7 @@ import DialogActions from "@mui/material/DialogActions";
 import Button from "@mui/material/Button";
 import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
 import {useEffect, useState} from "react";
+import Stack from "@mui/material/Stack";
 
 const DownloadSequencePopup = (props: {
   open: boolean, setOpen: (open: boolean) => void,
@@ -38,23 +38,23 @@ const DownloadSequencePopup = (props: {
     >
       <DialogTitle>Choose Sequence to Load:</DialogTitle>
       <DialogContent>
-        {displaySequences.map(info => {
-          return (
-            <Card>
-              <CardHeader>
-                <Typography variant="h6">{info.name}</Typography>
-              </CardHeader>
-              <CardContent>
-                <Typography variant="body2">{info.description}</Typography>
-              </CardContent>
-              <CardActions>
-                <IconButton type="submit" onClick={createHandler(info.id)}>
-                  <CheckCircleOutlineRoundedIcon/>
-                </IconButton>
-              </CardActions>
-            </Card>
-          );
-        })}
+        <Stack direction="column" spacing={1}>
+          {displaySequences.map(info => {
+            return (
+              <Card key={info.id}>
+                <CardContent>
+                  <Typography variant="h5">{info.name}</Typography>
+                  <Typography variant="body2">{info.description}</Typography>
+                </CardContent>
+                <CardActions>
+                  <IconButton type="submit" onClick={createHandler(info.id)}>
+                    <CheckCircleOutlineRoundedIcon/>
+                  </IconButton>
+                </CardActions>
+              </Card>
+            );
+          })}
+        </Stack>
       </DialogContent>
       <DialogActions>
         <Button key="cancel" onClick={e => {
