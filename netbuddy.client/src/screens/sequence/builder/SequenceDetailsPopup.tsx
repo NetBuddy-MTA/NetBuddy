e2ebac6f5afe10af0a49401a9787f8d42b765c84
OE100 <omer.er10@gmail.com>
2024-05-20 12:21:35 +0300

    added sequence details popup to allow editing sequence name and description.
    (and viewing the description)

---
+++ netbuddy.client/src/screens/sequence/builder/SequenceDetailsPopup.tsx
@@ -0,0 +1,90 @@
+﻿import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import IconButton from "@mui/material/IconButton";
+import Tooltip from "@mui/material/Tooltip";
+import {useEffect, useState} from "react";
+import CheckCircleOutlineRoundedIcon from '@mui/icons-material/CheckCircleOutlineRounded';
+import EditRoundedIcon from '@mui/icons-material/EditRounded';
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+
+const SequenceDetailsPopup = (props: {
+  open: boolean, setOpen: (open: boolean) => void,
+  name: string, setName: (name: string) => void,
+  description: string, setDescription: (description: string) => void,
+}) => {
+  const {
+    open, setOpen,
+    name, setName,
+    description, setDescription
+  } = props;
+  const [editMode, setEditMode] = useState<boolean>(false);
+  const [editName, setEditName] = useState<string>(name);
+  const [editDescription, setEditDescription] = useState<string>(description);
+
+  useEffect(() => {
+    setEditMode(false);
+    setEditName(name);
+    setEditDescription(description);
+  }, [open]);
+
+  return (
+    <Dialog
+      open={open}
+      onClose={() => setOpen(false)}
+    >
+      <DialogTitle>Sequence Details:</DialogTitle>
+      <DialogContent>
+        <Stack direction="column" spacing={2}>
+          <TextField
+            disabled={!editMode}
+            autoFocus
+            id="name"
+            name="name"
+            label="Name"
+            type="text"
+            variant="standard"
+            value={editName}
+            onChange={e => setEditName(e.target.value)}
+          />
+          <TextField
+            disabled={!editMode}
+            autoFocus
+            id="description"
+            name="description"
+            label="Description"
+            type="text"
+            multiline
+            variant="standard"
+            minRows={2}
+            maxRows={6}
+            value={editDescription}
+            onChange={e => setEditDescription(e.target.value)}
+          />
+        </Stack>
+      </DialogContent>
+      <DialogActions>
+        <Tooltip title={editMode ? "Save Changes" : "Edit"}>
+          <IconButton onClick={e => {
+            e.preventDefault();
+            if (editMode) {
+              setEditMode(false);
+              setName(editName);
+              setDescription(editDescription);
+            } else {
+              setEditName(name);
+              setEditDescription(description);
+              setEditMode(true);
+            }
+          }}>
+            {editMode ? <CheckCircleOutlineRoundedIcon/> : <EditRoundedIcon/>}
+          </IconButton>
+        </Tooltip>
+      </DialogActions>
+    </Dialog>
+  )
+};
+
+export default SequenceDetailsPopup;
