4b454cd9bc8ee61c0a5ea8eced1778cca70ce3a8
OE100 <omer.er10@gmail.com>
2024-05-29 20:09:50 +0300

    added dto for selector

---
+++ NetBuddy.Server/DTOs/Selector/SelectorDto.cs
@@ -0,0 +1,14 @@
+﻿using NetBuddy.Server.Models.Executables.Selector;
+
+namespace NetBuddy.Server.DTOs.Selector;
+
+public sealed class SelectorDto
+{
+    public string Id { get; set; }
+
+    public string Url { get; set; } = string.Empty;
+
+    public string Name { get; set; } = string.Empty;
+
+    public SelectorStage[] Stages { get; set; } = [];
+}
