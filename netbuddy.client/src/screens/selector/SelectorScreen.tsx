da0b10bd6daff8296675ad0a1d199be82b206901
OE100 <omer.er10@gmail.com>
2024-05-29 22:55:50 +0300

    added delete button. added refresh mechanic.

--- netbuddy.client/src/screens/selector/SelectorScreen.tsx
+++ netbuddy.client/src/screens/selector/SelectorScreen.tsx
@@ -1,5 +1,5 @@
 ﻿import {useEffect, useState} from "react";
-import {GetAllSelectors, Selector} from "../../api/selectors/selectors.ts";
+import {DeleteSelector, GetAllSelectors, Selector} from "../../api/selectors/selectors.ts";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
 import DialogContent from "@mui/material/DialogContent";
@@ -11,6 +11,7 @@ import SelectorList from "./list/SelectorList.tsx";
 import SelectorView from "./editor/SelectorView.tsx";
 import EditRoundedIcon from '@mui/icons-material/EditRounded';
 import Tooltip from "@mui/material/Tooltip";
+import DeleteIcon from '@mui/icons-material/Delete';
 
 export type SelectorScreenProps = {
   open: boolean;
@@ -27,11 +28,14 @@ const SelectorScreen = (props: SelectorScreenProps) => {
   const [urls, setUrls] = useState<string[]>([]);
   const [mode, setMode] = useState<"list" | "view">("list");
   const [save, setSave] = useState<boolean>(false)
+  const [refresh, setRefresh] = useState<boolean>(false);
 
   useEffect(() => {
-    // update selector list
+    if (!refresh && !open) return;
+    setSelector(undefined);
     GetAllSelectors().then(setSelectors);
-  }, [open]);
+    setRefresh(false);
+  }, [refresh, open]);
 
   useEffect(() => {
     // set urls to unique urls from selectors
@@ -51,7 +55,7 @@ const SelectorScreen = (props: SelectorScreenProps) => {
       <DialogTitle>
         {mode === "list" ? "Selectors" : "Edit Selector"}
       </DialogTitle>
-      <DialogContent>
+      <DialogContent style={{overflow: "hidden"}}>
         {
           mode === "list" ?
             <SelectorList urls={urls} selectors={selectors} choice={selector} setChoice={setSelector}/> :
@@ -62,6 +66,12 @@ const SelectorScreen = (props: SelectorScreenProps) => {
         <Tooltip title={mode === "view" ? "Cancel" : "Close"}>
           <IconButton onClick={() => mode === "view" ? setMode("list") : onClose()}>
             <CancelIcon/>
+          </IconButton>
+        </Tooltip>
+        <Tooltip title="Delete Selection">
+          <IconButton
+            onClick={() => selector !== undefined && DeleteSelector(selector.id).then(() => setRefresh(true))}>
+            <DeleteIcon/>
           </IconButton>
         </Tooltip>
         {
