a5130cee0df2b8b6dcd1e66324c7ca3bd787eaa3
OE100 <omer.er10@gmail.com>
2024-06-03 19:17:47 +0300

    added the action result model

---
+++ NetBuddy.Server/Models/History/ActionResult.cs
@@ -0,0 +1,11 @@
+﻿using NetBuddy.Server.Models.Executables.Action;
+using Action = System.Action;
+
+namespace NetBuddy.Server.Models.History;
+
+public sealed class ActionResult
+{
+    public Action Action { get; set; }
+
+    public Dictionary<Variable, object> ActionContext { get; set; }
+}
