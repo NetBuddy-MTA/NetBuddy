4cb778672395bd00e19d68f2e214f074199c9f5a
Dana Chesner <dana.chesner@gmail.com>
2024-06-08 19:57:33 +0300

    added UrlInput.tsx

---
+++ netbuddy.client/src/screens/sequence/execution/inputs/UrlInput.tsx
@@ -0,0 +1,46 @@
+import {FC, useState} from 'react';
+import {InputProps} from "./types.ts";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
+export const UrlInput: FC<InputProps<string>>  = ({field, defaultValue,onChange, title}) => {
+  const [value, setValue] = useState<string>(defaultValue);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    setValue(value)
+    
+    if(!value) {
+      setError(null)
+      return
+    }
+    
+    if (value && value.match(/^(http|https):\/\/[^ "]+$/)) {
+      onChange(field, value);
+      setError(null)
+    }
+    else {
+      setError("Invalid URL")
+    }
+  }
+  
+  return (
+    <Box sx={{ mb: 2 }}>
+      <Typography variant="body1" mr={1}>
+        {title}
+      </Typography>
+      <TextField
+        id="url-input"
+        type="url"
+        value={value}
+        onChange={handleOnChange}
+        error={!!error}
+        helperText={error}
+        fullWidth
+        variant="outlined"
+      />
+    </Box>
+  );
+}
