c42e81fe3d4a56c5d353da7924626305c05d99bf
Dana Chesner <dana.chesner@gmail.com>
2024-06-08 19:56:03 +0300

    added types.ts

---
+++ netbuddy.client/src/screens/sequence/execution/inputs/types.ts
@@ -0,0 +1,15 @@
+export const INPUTS_TYPES = ["string", "number", "url", "boolean" , "unknown"] as const;
+export type InputType = typeof INPUTS_TYPES[number];
+
+export type InputProps<T = any> = {
+  title: string;
+  onChange: (field: string, value: T) => void;
+  // onChange: (action: string, inputString: string, value: T) => void;
+  required: boolean;
+  defaultValue: T;
+  field: string;
+
+  // action: string;
+  // inputString: string
+}
+
