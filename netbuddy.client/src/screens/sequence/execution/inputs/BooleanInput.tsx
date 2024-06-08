f16ceec24f59d337119c451505876627f6024300
Dana Chesner <dana.chesner@gmail.com>
2024-06-08 19:57:47 +0300

    added BooleanInput.tsx

---
+++ netbuddy.client/src/screens/sequence/execution/inputs/BooleanInput.tsx
@@ -0,0 +1,31 @@
+import {FC, useState} from 'react';
+import {InputProps} from "./types.ts";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Checkbox from "@mui/material/Checkbox";
+
+export const BooleanInput: FC<InputProps<boolean>>  = ({field, defaultValue, onChange, title}) => {
+  const [value, setValue] = useState<boolean>(defaultValue);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = Boolean(e.target.value);
+    onChange(field, value);
+    setValue(value);
+  }
+
+  return (
+    <Box sx={{ mb: 2 }}>
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={value}
+            onChange={handleOnChange}
+            color="primary"
+          />
+        }
+        label={<Typography variant="body1">{title}</Typography>}
+      />
+    </Box>
+  );
+}
