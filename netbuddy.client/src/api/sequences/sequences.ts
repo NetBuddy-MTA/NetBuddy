f2d49b0551989ff646e67534512893f037777685
Dana Chesner <dana.chesner@gmail.com>
2024-06-08 20:00:36 +0300

    changed type to SequenceVariableType instead of string

--- netbuddy.client/src/api/sequences/sequences.ts
+++ netbuddy.client/src/api/sequences/sequences.ts
@@ -6,11 +6,14 @@ export type SequenceDisplay = {
   description: string;
 };
 
+export type SequenceVariableType = "Tab" | "Window" | "URL" | "String" | "HttpResponse" | "Variable" | 
+    "?" | "Boolean" |"Element" | "Number" | "Element[]" | "Selector" ;
+
 export type SequenceVariable = {
   originalName: string;
   name: string;
   description: string;
-  type: string;
+  type: SequenceVariableType;
   optional: boolean;
   defaultValue?: string;
 };
