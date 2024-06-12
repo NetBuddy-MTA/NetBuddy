765fddf150df6af13c4a037b61fe592760fafd69
Dana Chesner <dana.chesner@gmail.com>
2024-06-12 21:18:22 +0300

    Added an option to delete a text field

--- netbuddy.client/src/screens/sequence/execution/inputs/StringArrInput.tsx
+++ netbuddy.client/src/screens/sequence/execution/inputs/StringArrInput.tsx
@@ -5,6 +5,9 @@ import Typography from "@mui/material/Typography";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
+import {InputAdornment} from "@mui/material";
+import IconButton from "@mui/material/IconButton";
+import DeleteIcon from "@mui/icons-material/Delete";
 
 export const StringArrInput = ({ field, defaultValue, onChange, title, required }: InputProps) => {
   const [values, setValues] = useState<string[]>([defaultValue || '']);
@@ -19,6 +22,12 @@ export const StringArrInput = ({ field, defaultValue, onChange, title, required 
 
   const handleAddField = () => {
     setValues([...values, '']);
+  };
+
+  const handleRemoveField = (index: number) => () => {
+    const updatedValues = values.filter((_, i) => i !== index);
+    setValues(updatedValues);
+    onChange(field, updatedValues);
   };
 
   return (
@@ -37,6 +46,15 @@ export const StringArrInput = ({ field, defaultValue, onChange, title, required 
           fullWidth
           variant="outlined"
           sx={{ mb: 1 }}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <IconButton onClick={handleRemoveField(index)} aria-label="delete">
+                  <DeleteIcon />
+                </IconButton>
+              </InputAdornment>
+            ),
+          }}
         />
       ))}
       <Button variant="contained" onClick={handleAddField}>
