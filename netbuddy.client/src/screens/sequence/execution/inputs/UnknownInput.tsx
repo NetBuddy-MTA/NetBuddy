211b1d9c3a30df8c8e95c57e32b6a81e887b6174
Dana Chesner <dana.chesner@gmail.com>
2024-06-12 17:16:22 +0300

    Created UnknownInput.tsx

---
+++ netbuddy.client/src/screens/sequence/execution/inputs/UnknownInput.tsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+import {InputProps} from "./types.ts";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import TextField from "@mui/material/TextField";
+
+export const UnknownInput = ({field, defaultValue, onChange, title, required, disabled}: InputProps) => {
+  const [value, setValue] = useState<string | undefined>(defaultValue);
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    onChange(field, value);
+    setValue(value);
+  }
+
+  return (
+    <Box sx={{ mb: 2 }}>
+      <Typography variant="body1" mr={1}>
+        {title}
+      </Typography>
+      <TextField
+        id="string-input"
+        type="string"
+        value={value}
+        onChange={handleOnChange}
+        required={required}
+        defaultValue={defaultValue}
+        disabled={disabled}
+        fullWidth
+        variant="outlined"
+      />
+    </Box>
+  );
+}
