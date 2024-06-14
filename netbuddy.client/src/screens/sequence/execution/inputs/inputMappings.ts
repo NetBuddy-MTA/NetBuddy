e451a8f433aed29a8e140c0aa5ccc7b65a821f77
Dana Chesner <dana.chesner@gmail.com>
2024-06-14 17:43:26 +0300

    Removed input types that the user can't fill

--- netbuddy.client/src/screens/sequence/execution/inputs/inputMappings.ts
+++ netbuddy.client/src/screens/sequence/execution/inputs/inputMappings.ts
@@ -4,15 +4,12 @@ import {UrlInput} from "./UrlInput.tsx";
 import {NumberInput} from "./NumberInput.tsx";
 import {StringInput} from "./StringInput.tsx";
 import {BooleanInput} from "./BooleanInput.tsx";
-import {OptionsInput} from "./OptionsInput.tsx";
 
 const mapSequenceVarToInput: Record<string, (props: InputProps) => ReactNode> = {
   "String": StringInput,
   "Number": NumberInput,
   "URL": UrlInput,
   "Boolean": BooleanInput,
-  "Tab": OptionsInput,
-  "Window": OptionsInput,
 };
 
 export default mapSequenceVarToInput;
