38ec12bdbe54cb45b1e54433200fe1bf371ce887
OE100 <omer.er10@gmail.com>
2024-06-18 13:24:43 +0300

    removed duplicate algorithm
    added relevant inputs to props

--- netbuddy.client/src/screens/sequence/execution/ExecutionButton.tsx
+++ netbuddy.client/src/screens/sequence/execution/ExecutionButton.tsx
@@ -1,45 +1,25 @@
 import Button from "@mui/material/Button";
 import {useMemo} from "react";
-import {Sequence, SequenceVariable} from "../../../api/sequences/sequences.ts";
-import mapSequenceVarToInput from "./inputs/inputMappings.ts";
+import {Sequence} from "../../../api/sequences/sequences.ts";
 
 type Props = {
   sequence: Sequence;
   values: Record<string, any>;
+  inputs: string[];
 }
 
-export const ExecutionButton = ({sequence, values}: Props) => {
-  const handleExecute =() => {
+export const ExecutionButton = ({sequence, values, inputs}: Props) => {
+  const handleExecute = () => {
     // todo implement execution
   }
-  
-  const requiredFields = useMemo(() => {
-    if (!sequence) return new Set<string>();
-    const seen = new Set<string>();
-    const requiredFields = new Set<string>();
-
-    sequence.actions.forEach(action => {
-      const relevant = (input: SequenceVariable) =>
-        !seen.has(input.name) && input.type.split("[]", 1).some(t => t in mapSequenceVarToInput);
-      const relevantInputs = action.inputs.filter(relevant);
-
-      relevantInputs.forEach(input => {
-        if (!input.optional) {
-          requiredFields.add(action.actionString + "|" + input.originalName);
-          seen.add(input.name);
-        }
-      });
-      action.outputs.forEach(output => seen.add(output.name));
-    });
-    return requiredFields;
-  }, [sequence]);
 
   const allRequiredFieldsFilled = useMemo(() => {
-    return Array.from(requiredFields).every(field => values[field] !== undefined && values[field] !== "");
-  }, [requiredFields, values]);
-  
+    return Array.from(inputs).every(input => input in values);
+  }, [values, inputs]);
+
   return (
-    <Button variant="contained" color="primary" size="large" onClick={handleExecute} disabled={!allRequiredFieldsFilled}>
+    <Button variant="contained" color="primary" size="large" onClick={handleExecute}
+            disabled={!allRequiredFieldsFilled}>
       Execute
     </Button>
   );
