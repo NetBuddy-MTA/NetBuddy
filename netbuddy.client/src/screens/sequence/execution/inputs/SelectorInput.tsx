ebfb3be6bed946614070ac49b5bffb8016510dc2
OE100 <omer.er10@gmail.com>
2024-06-18 17:57:08 +0300

    changed selector input to work with new parameters

--- netbuddy.client/src/screens/sequence/execution/inputs/SelectorInput.tsx
+++ netbuddy.client/src/screens/sequence/execution/inputs/SelectorInput.tsx
@@ -6,24 +6,22 @@ import {InputProps} from "./types.ts";
 import {Selector} from "../../../../api/selectors/selectors.ts";
 import Box from "@mui/material/Box";
 
-export const SelectorInput = ({title, defaultValue}: InputProps) => {
-  const [value, setValue] = useState<Selector | undefined>(defaultValue);
+export const SelectorInput = ({title, defaultValue, setValue}: InputProps) => {
+  const [selector, setSelector] = useState<Selector | undefined>(defaultValue);
   const [isOpen, setOpen] = useState<boolean>(false);
-  
-  const handleSetSelection = (selector?: Selector) => {
-    setValue(selector);
-    setOpen(false);
-  };
-  
+
   return (
     <Box>
       <Typography variant="body1">{title}</Typography>
       <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
         Select a selector
       </Button>
-      {value && (
-        <Typography variant="body1" mr={1}>Selected selector: {value?.name}</Typography>)}
-      <SelectorScreen open={isOpen} onClose={() => setOpen(false)} setSelection={handleSetSelection} />
+      {selector && (
+        <Typography variant="body1" mr={1}>Selected selector: {selector?.name}</Typography>)}
+      <SelectorScreen open={isOpen} onClose={() => setOpen(false)} setSelection={selector => {
+        setSelector(selector);
+        setValue(selector);
+      }}/>
     </Box>
   );
 };
