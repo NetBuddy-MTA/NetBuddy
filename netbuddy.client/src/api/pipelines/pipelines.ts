4426b6b4f171d9dfc1021306bda1b1b568a29b42
OE100 <omer.er10@gmail.com>
2024-06-18 15:48:29 +0300

    added client api for adding a pipeline to the run queue

---
+++ netbuddy.client/src/api/pipelines/pipelines.ts
@@ -0,0 +1,20 @@
+﻿import {Sequence} from "../sequences/sequences.ts";
+import agent from "../agent.ts";
+
+export type Pipeline = {
+  id: string;
+  sequence: Sequence;
+  context: Record<string, any>;
+  isRunning: boolean;
+  isFinished: boolean;
+};
+
+export async function PutPipeline(pipeline: Pipeline) {
+  return await agent
+  .put<string>("/execution/queue", JSON.stringify({
+    ...pipeline,
+    id: pipeline.id === "" ? "00000000-0000-0000-0000-000000000000" : pipeline.id
+  }))
+  .catch(error => console.log(error))
+  .then(response => response?.data)
+}
