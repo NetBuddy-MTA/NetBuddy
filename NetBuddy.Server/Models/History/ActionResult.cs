14b4ee15f6b5d1db74d5f1f5bea0f60aa538de60
OE100 <omer.er10@gmail.com>
2024-08-11 15:11:09 +0300

    new models for action and sequence results wip

--- NetBuddy.Server/Models/History/ActionResult.cs
+++ NetBuddy.Server/Models/History/ActionResult.cs
@@ -1,11 +1,17 @@
 ﻿using NetBuddy.Server.Models.Executables.Action;
-using Action = System.Action;
+using Action = NetBuddy.Server.Models.Executables.Action.Action;
 
 namespace NetBuddy.Server.Models.History;
 
 public sealed class ActionResult
 {
+    public DateTime EndAt;
+
     public Action Action { get; set; }
 
     public Dictionary<Variable, object> ActionContext { get; set; }
+
+    public List<KeyValuePair<string, string>> ActionLogs { get; set; }
+
+    public Dictionary<Variable, object> ActionOutputs { get; set; }
 }
---
+++ NetBuddy.Server/Models/History/SequenceResult.cs
@@ -0,0 +1,21 @@
+﻿using Marten.Schema;
+
+namespace NetBuddy.Server.Models.History;
+
+public class SequenceResult
+{
+    // End time
+    public DateTime EndAt;
+
+    // List of Action results
+    public List<ActionResult> Results;
+
+    // Start time
+    public DateTime StartAt;
+
+    // Id is the primary key
+    [Identity] public Guid Id { get; set; }
+
+    // Sequence id
+    public Guid SequenceId { get; set; }
+}
