a7d17e690528b43a46ee37adade0f6e608e885bd
OE100 <omer.er10@gmail.com>
2024-08-18 17:11:28 +0300

    modified range dto to be assignable

--- NetBuddy.Server/DTOs/Range/Range.cs
+++ NetBuddy.Server/DTOs/Range/Range.cs
@@ -1,10 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿namespace NetBuddy.Server.DTOs.Range;
 
-namespace NetBuddy.Server.DTOs.Range;
-
-[BindProperties]
 public class Range
 {
-    public int From;
-    public int To;
+    public int From { get; set; }
+    public int To { get; set; }
 }
