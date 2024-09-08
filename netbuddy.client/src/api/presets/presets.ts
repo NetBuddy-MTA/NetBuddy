533cee27b22804bf9b205fd8156c1dc936879a20
OE100 <omer.er10@gmail.com>
2024-09-08 08:58:04 +0300

    added preset api functions to frontend

---
+++ netbuddy.client/src/api/presets/presets.ts
@@ -0,0 +1,26 @@
+﻿import agent from "../agent.ts";
+
+export type Preset = {
+  id: string;
+  name: string;
+  description: string;
+  sequenceId: string;
+  context: Record<string, string>;
+};
+
+export async function GetPresets(sequenceId: string) {
+  return agent.get<Preset[]>("/execution/presets/", {params: {sequenceId}})
+  .catch(error => console.log(error))
+  .then(response => response?.data);
+}
+
+export async function PutPreset(preset: Preset) {
+  return agent.put("/execution/presets/save", JSON.stringify({
+    ...preset,
+    id: preset.id === "" ? "00000000-0000-0000-0000-000000000000" : preset.id
+  }));
+}
+
+export async function DeletePreset(presetId: string) {
+  return agent.delete("/execution/presets/delete/", {params: {presetId}});
+}
