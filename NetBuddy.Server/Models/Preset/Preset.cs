37abc0862fa9bf6d665f17b0cc602d19e281a746
OE100 <omer.er10@gmail.com>
2024-09-08 08:47:01 +0300

    added preset model

---
+++ NetBuddy.Server/Models/Preset/Preset.cs
@@ -0,0 +1,19 @@
+﻿using Marten.Schema;
+using NetBuddy.Server.Models.User;
+
+namespace NetBuddy.Server.Models.Preset;
+
+public sealed class Preset
+{
+    [Identity] public Guid Id;
+
+    public string Name { get; set; }
+
+    public string Description { get; set; }
+
+    public Guid SequenceId { get; set; }
+
+    public Dictionary<string, string> Context { get; set; } = new();
+
+    public UserAccount? Owner { get; set; }
+}
